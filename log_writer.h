82bbd3245e9d101ed42c5d242119e22b1973d3ad
陈 A Z <chenanzhuo517@gmail.com>
2022-05-06 22:39:22 +0800

    Update log_writer.h

--- log_writer.h
+++ log_writer.h
@@ -1,55 +1,119 @@
-﻿#ifndef MAINWINDOW_H
-#define MAINWINDOW_H
+#pragma once
 
-#include <QVBoxLayout>
-#include <QHBoxLayout>
-#include <QTableWidget>
-#include <QTableWidgetItem>
-#include <QLineEdit>
-#include <QPushButton>
-#include <QMainWindow>
-#include <QComboBox>
+#ifndef __H_LOG_WRITER_H__
+#define __H_LOG_WRITER_H__
 
-QT_BEGIN_NAMESPACE
-namespace Ui { class MainWindow; }
-QT_END_NAMESPACE
+// write program log
+// Add by humorly 2017/7/21
 
-enum
+#include <ctime>
+#include <cassert>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <mutex>
+
+namespace wstd
 {
-    invalid_table_type = -1,
-    department_table_type = 0,
-    staff_table_type = 1,
-    resign_staff_table_type = 2,
-};
+#define __FILE_LINE__	__FILE__, __LINE__
 
-enum
-{
-    invalid_index = -1,
-};
+	// std::size_t to string
+	inline std::string to_string(std::size_t value);
 
-class MainWindow : public QMainWindow
-{
-    Q_OBJECT
+	class log_writer;
+	// log writer set
+	class log_writer
+	{
+	public:
+		log_writer() {}
+		virtual ~log_writer() 
+		{
+			if (!mutex_.try_lock())
+			{
+				mutex_.unlock();
+			}
+		}
 
-public:
-    MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
+		// lock
+		void lock() 
+		{
+			mutex_.lock();
+		}
 
+		// unlock
+		void unlock()
+		{
+			mutex_.unlock();
+		}
 
+		// write log
+		static log_writer & log_store(const std::string str, const char * file, int line)
+		{
+			static log_writer log_;
+			log_.lock();
+			log_.write(str, file, line);
+			log_.unlock();
+			return log_;
+		}
 
-private slots:
+	private:
 
+		// 创建互斥锁
+		std::mutex mutex_;
 
-private:
-    Ui::MainWindow *ui;
-    // 整体布局
-    QVBoxLayout* hole_layout_;
-    // 表格界面
-    QTableWidget* content_table_;
-    // 表格类型
-    int table_type_;
+		enum
+		{
+			invalid_ptr = 0,
+		};
 
-    // 搜索内容
-    QLineEdit* search_;
-};
-#endif // MAINWINDOW_H
+		// write log
+		void write(const std::string & str, const char * file = __FILE__, int line = __LINE__) 
+		{
+
+			// get current time now
+			std::time_t time_ = std::time(reinterpret_cast<time_t *>(invalid_ptr));
+			tm * tm_ = std::localtime(&time_);
+			assert(tm_);
+
+			// make file name
+			std::string name_ = std::string("log_writer_") + wstd::to_string(tm_->tm_year + 1900) + std::string("-") +
+				wstd::to_string(tm_->tm_mon + 1) + std::string("-") + wstd::to_string(tm_->tm_mday) + std::string(".log");
+
+			// make now time
+			std::string now_time_ = wstd::to_string(tm_->tm_year + 1900) + std::string("-") +
+				wstd::to_string(tm_->tm_mon + 1) + std::string("-") + wstd::to_string(tm_->tm_mday) + std::string("-") +
+				wstd::to_string(tm_->tm_hour) + std::string(":") + wstd::to_string(tm_->tm_min) + std::string(":")
+				+ wstd::to_string(tm_->tm_sec);
+
+			// cut file name
+			std::string file_(file);
+			std::size_t npos_ = file_.rfind("\\");
+			if (std::string::npos != npos_) 
+			{
+				file_ = file_.substr(npos_);
+			}
+
+			// write log to file
+			std::ofstream of_(name_.c_str(), std::ios::out | std::ios::app);
+			if (of_.is_open())
+			{
+				of_ << "[log start. ";
+				of_ << now_time_ << ". ";
+				of_ << "" << file_ << ". " << "line " << line << "]";
+				of_ << " " << str << "\n";
+				of_.close();
+			}
+		}
+	};
+
+	// std::size_t to string
+	inline std::string to_string(std::size_t value)
+	{
+		char num_[128] = { 0 };
+		std::memset(num_, 0, sizeof(num_));
+		sprintf(num_, "%02zd", value);
+		return std::string(num_);
+	}
+}
+
+#endif // !__H_LOG_WRITER_H__
